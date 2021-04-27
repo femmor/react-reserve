b8fe7f18b83c47207953db72714d2745eb73e1d6
Emmanuel Egomson <fegomson@gmail.com>
2021-04-27 18:49:33 -0500

    Implemented getInitialProps and passed to the Component

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,32 @@
 import React, {useEffect, useState} from "react"
 import axios from 'axios'
 
-function Home() {
-  useEffect(() => {
-    getProducts()
-  }, [])
+function Home({ products }) {
+  console.log(products)
+  // useEffect(() => {
+  //   getProducts()
+  // }, [])
 
-  async function getProducts() {
-    const url = 'http://localhost:3000/api/products'
-    const response = await axios.get(`${url}`)
-    console.log(response.data)
-  }
+  // async function getProducts() {
+  //   const url = 'http://localhost:3000/api/products'
+  //   const response = await axios.get(`${url}`)
+  //   console.log(response.data)
+  // }
 
 
   return <>home</>;
+}
+
+// getInitialProps
+Home.getInitialProps = async () => {
+  // fetch data on the server
+  // return response data as an object
+  // note: this object will be merged with existing props
+  const url = 'http://localhost:3000/api/products'
+  const response = await axios.get(`${url}`)
+  return {
+    products: response.data
+  }
 }
 
 export default Home;
