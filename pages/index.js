136f4e8039f74176b9f9dec70e052751ee392b82
Emmanuel Egomson <fegomson@gmail.com>
2021-04-28 09:22:25 -0500

    Configured MongoAtlas and connected to the Database

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React from "react"
 import axios from 'axios'
 
 function Home({ products }) {
@@ -12,7 +12,6 @@ function Home({ products }) {
   //   const response = await axios.get(`${url}`)
   //   console.log(response.data)
   // }
-
 
   return <>home</>;
 }
