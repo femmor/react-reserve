6aeb9f8d99c64251f59fbc3f5efed5e049ccf953
Emmanuel Egomson <fegomson@gmail.com>
2021-04-29 15:39:04 -0500

    Get products by ID, Style Product Details Page

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,5 +1,21 @@
+import { Input } from "semantic-ui-react";
+
 function AddProductToCart() {
-  return <>AddProductToCart</>;
+  return (
+    <>
+      <Input 
+        value="1"
+        type="number"
+        min="1"
+        placeholder="Quantity"
+        action={{ 
+          color: "orange", 
+          content: "Add to Cart",
+          icon: "plus cart"
+        }}
+      />
+    </>
+  );
 }
 
 export default AddProductToCart;
