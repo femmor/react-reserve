626d2c4a2e2a3c9b0cdd2935963b0b80d94047ce
Emmanuel Egomson <fegomson@gmail.com>
2021-04-29 17:13:38 -0500

    Mnaged the created form state and displayed uploaded image

--- pages/create.js
+++ pages/create.js
@@ -1,12 +1,38 @@
+import React, {useState} from 'react'
 import { Form, Input, TextArea, Button, Image, Message, Header, Icon } from 'semantic-ui-react'
 
 function CreateProduct() {
+  const [product, setProduct] = useState({
+    name: '',
+    price: '',
+    media: '',
+    description: ''
+  })
+
+  const [mediaPreview, setMediaPreview] = useState('')
+
+  const handleChange = (e) => {
+    const {name, value, files} = e.target
+    // For media
+    if (name === 'media') {
+      setProduct((...prevState) => ({
+        ...prevState,
+        media : files[0]
+      }))
+      setMediaPreview(window.URL.createObjectURL(files[0]))
+    } else {
+      setProduct((...prevState) => ({
+        ...prevState,
+        [name] : value
+      }))
+    }
+  }
 
   return (
     <>
       <Header as="h2" block>
         <Icon name="add" color="orange"/>
-        Create a new product
+        Create new product
       </Header>
       <Form>
         <Form.Group widths="equal">
@@ -16,6 +42,7 @@ function CreateProduct() {
             label="Name"
             placeholder="Name"
             type="text"
+            onChange={handleChange}
           />
           <Form.Field 
             control={Input}
@@ -25,6 +52,7 @@ function CreateProduct() {
             min="0.00"
             step="0.01"
             type="number"
+            onChange={handleChange}
           />
           <Form.Field 
             control={Input}
@@ -33,13 +61,16 @@ function CreateProduct() {
             type="file"
             accept="image/*"
             content="Select Image"
+            onChange={handleChange}
           />
         </Form.Group>
+        <Image src={mediaPreview} rounded centered size="small"/>
         <Form.Field
           control={TextArea}
           name="description"
           label="Description"
           placeholder="Description"
+          onChange={handleChange}
         />
         <Form.Field
           control={Button}
